7d4905e1b37f4a346fcc26f40975189c637174ed
Lion447 <113065883+Lion447@users.noreply.github.com>
2026-08-02 14:56:00 -0300

    Modificaciones

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,9 +1,11 @@
 // Botton volver arriba
 const btn = document.querySelector(".scroll-up-btn");
 
-btn.addEventListener("click", () => {
-    document.documentElement.scrollTo({
-        top: 0, behavior: "smooth",
+if (btn) {
+    btn.addEventListener("click", () => {
+        document.documentElement.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
     });
-});
-
+}
